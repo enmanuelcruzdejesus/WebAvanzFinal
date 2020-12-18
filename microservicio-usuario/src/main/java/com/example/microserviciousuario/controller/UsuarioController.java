599a7601aad7681f756595987c6495c6b1c75fc5
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-12-18 14:30:39 -0400

    adding monitorserver

--- microservicio-usuario/src/main/java/com/example/microserviciousuario/controller/UsuarioController.java
+++ microservicio-usuario/src/main/java/com/example/microserviciousuario/controller/UsuarioController.java
@@ -1,25 +1,15 @@
 package com.example.microserviciousuario.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Logger;
-
 @RestController
-@RequestMapping("/")
-public class AppController {
-
-
-    @Value("${server.port}")
-    private int puerto;
+@RequestMapping("/usuario/")
+public class UsuarioController {
 
     @GetMapping("/")
     public String index(){
-
-
-        return "hello from microservicio usuario en el puerto = "+puerto;
-
+        return "Usuario controller ";
     }
 }
